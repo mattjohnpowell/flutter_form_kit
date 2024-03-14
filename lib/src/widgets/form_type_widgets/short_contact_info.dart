5bfd78ccd7fdcf33cdf17918dd2e6a5b8a36c83f
Matt <mattjohnpowell@hotmail.com>
2024-03-14 16:16:34 +0000

    Update ShortContactInfo widget and pubspec.yaml version

--- lib/src/widgets/form_type_widgets/short_contact_info.dart
+++ lib/src/widgets/form_type_widgets/short_contact_info.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_kit/src/widgets/flutter_form_details.dart';
 
-
 class ShortContactInfo extends StatefulWidget {
   final Function(List<String>) fields;
 
@@ -13,16 +12,20 @@ class ShortContactInfo extends StatefulWidget {
 
 class _ShortContactInfoState extends State<ShortContactInfo> {
   final TextEditingController controller1 = TextEditingController();
-  final TextEditingController controller4 = TextEditingController(); // Assuming this is the email controller
+  final TextEditingController controller4 =
+      TextEditingController(); // Assuming this is the email controller
+  final TextEditingController controllerBirthday = TextEditingController();
+  final TextEditingController controllerHeight = TextEditingController();
 
   // Use ValueNotifier to manage the validation state of the email
   ValueNotifier<bool> isValidEmail = ValueNotifier(true);
 
   @override
   void dispose() {
-    // Dispose controllers and ValueNotifier when the widget is disposed
     controller1.dispose();
     controller4.dispose();
+    controllerBirthday.dispose();
+    controllerHeight.dispose();
     isValidEmail.dispose();
     super.dispose();
   }
@@ -32,12 +35,39 @@ class _ShortContactInfoState extends State<ShortContactInfo> {
     return Column(
       children: [
         contactInfoTextField("First name", "Jane", context, controller1),
-        contactInfoTextField("Email", "name@example.com", context, controller4, isEmail: true),
+        contactInfoTextField("Email", "name@example.com", context, controller4,
+            isEmail: true),
+        GestureDetector(
+          onTap: () => _selectDate(context),
+          child: AbsorbPointer(
+            child: contactInfoTextField("Birthday", "Select your birthday",
+                context, controllerBirthday),
+          ),
+        ),
+        contactInfoTextField(
+            "Height (cm)", "Enter your height", context, controllerHeight,
+            isNumber: true),
       ],
     );
   }
 
-  Widget contactInfoTextField(String label, String hint, BuildContext context, TextEditingController controller, {bool isEmail = false}) {
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != DateTime.now()) {
+      setState(() {
+        controllerBirthday.text = "${picked.toLocal()}".split(' ')[0];
+      });
+    }
+  }
+
+  Widget contactInfoTextField(String label, String hint, BuildContext context,
+      TextEditingController controller,
+      {bool isEmail = false, bool isNumber = false}) {
     final themeColor = FlutterFormDetails.of(context).themeColor;
     final textTheme = Theme.of(context).textTheme;
 
@@ -53,9 +83,13 @@ class _ShortContactInfoState extends State<ShortContactInfo> {
               if (isEmail) {
                 isValidEmail.value = _validateEmail(value);
               }
+              // Update fields for the ShortContactInfo widget
               widget.fields([
                 controller1.text,
                 controller4.text,
+                // Include new controllers' values
+                controllerBirthday.text,
+                controllerHeight.text,
               ]);
             },
             cursorColor: themeColor,
@@ -66,11 +100,14 @@ class _ShortContactInfoState extends State<ShortContactInfo> {
               floatingLabelAlignment: FloatingLabelAlignment.start,
               hintText: hint,
               focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: themeColor)
-              ),
-              fillColor: !isValid && isEmail ? Colors.red.withOpacity(0.2) : null,
+                  borderSide: BorderSide(color: themeColor)),
+              fillColor:
+                  !isValid && isEmail ? Colors.red.withOpacity(0.2) : null,
               filled: !isValid && isEmail,
             ),
+            keyboardType: isNumber
+                ? TextInputType.numberWithOptions(decimal: true)
+                : TextInputType.text,
           );
         },
       ),
